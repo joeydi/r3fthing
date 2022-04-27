03912e7c778996e4f03de6aadb5fd32552650809
Joe di Stefano <joeydi@okaypl.us>
2022-04-27 15:27:12 -0400

    add unsplash search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import React, { useRef, useState } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
@@ -18,39 +18,77 @@ function Box(props) {
 
     return (
         <mesh {...props} ref={mesh} scale={active ? 1.5 : 0.5} onClick={() => setActive(!active)} onPointerOver={() => setHover(true)} onPointerOut={() => setHover(false)}>
-            <boxGeometry args={[1.4, 1, 0.1]} />
+            <boxGeometry args={[1, 1, 1]} />
             <meshStandardMaterial map={props.map} />
         </mesh>
     );
 }
 
 function App() {
-    const rows = 20;
-    const columns = 20;
+    const defaultTextures = ["textures/1.jpg", "textures/2.jpg", "textures/3.jpg", "textures/4.png", "textures/5.jpg", "textures/6.jpg"];
 
-    const textures = useLoader(TextureLoader, ["textures/1.jpg", "textures/2.jpg", "textures/3.jpg", "textures/4.png", "textures/5.jpg", "textures/6.jpg"]);
+    const [width, setWidth] = useState(10);
+    const [height, setHeight] = useState(10);
+    const [depth, setDepth] = useState(10);
+    const [query, setQuery] = useState("");
+    const [textures, setTextures] = useState(defaultTextures);
+
+    const loadedTextures = useLoader(TextureLoader, textures);
 
     const positions = [];
-    for (let y = 0; y < rows; y++) {
-        for (let x = 0; x < columns; x++) {
-            positions.push([gsap.utils.mapRange(0, columns, -8, 8, x), gsap.utils.mapRange(0, rows, -6, 6, y), 0]);
+    for (let z = 0; z < depth; z++) {
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                positions.push([gsap.utils.mapRange(0, width, -8, 8, x), gsap.utils.mapRange(0, height, -8, 8, y), gsap.utils.mapRange(0, depth, -8, 8, z)]);
+            }
         }
     }
 
-    console.log(positions);
+    const handleSearch = (e) => {
+        e.preventDefault();
+        fetch(`https://api.unsplash.com/search/photos?query=${query}&per_page=19&client_id=IxRD9wlbmoqRRf2LXCc-iurxRGsNKo8zcloqCJQmaGQ`)
+            .then((response) => response.json())
+            .then((data) => {
+                setTextures(data.results.map((result) => result.urls.regular));
+            });
+    };
 
     return (
-        <Canvas>
-            <ambientLight intensity={0.5} />
-            <pointLight position={[10, 10, 100]} />
+        <>
+            <header>
+                <div>
+                    <label>Width</label>
+                    <br />
+                    <input type="range" min="1" max="15" step="1" onChange={(e) => setWidth(e.target.value)} />
+                </div>
+                <div>
+                    <label>Height</label>
+                    <br />
+                    <input type="range" min="1" max="15" step="1" onChange={(e) => setHeight(e.target.value)} />
+                </div>
+                <div>
+                    <label>Depth</label>
+                    <br />
+                    <input type="range" min="1" max="15" step="1" onChange={(e) => setDepth(e.target.value)} />
+                </div>
+                <form onSubmit={handleSearch}>
+                    <label>Search</label>
+                    <br />
+                    <input type="text" onChange={(e) => setQuery(e.target.value)} />
+                    <input type="submit" style={{ marginLeft: "10px" }} />
+                </form>
+            </header>
+            <Canvas>
+                <ambientLight intensity={0.5} />
+                <pointLight position={[10, 10, 10]} />
 
-            {positions.map((position, i) => {
-                console.log(positions[i]);
-                return <Box key={i} position={position} map={gsap.utils.wrap(textures, i)} />;
-            })}
+                {positions.map((position, i) => {
+                    return <Box key={i} position={position} map={gsap.utils.wrap(loadedTextures, i)} />;
+                })}
 
-            <OrbitControls />
-        </Canvas>
+                <OrbitControls />
+            </Canvas>
+        </>
     );
 }
 
